4061158f4f725b373910b6607377401a57122f59
Abraham Sulit <abraham.sulit@yahoo.com>
2026-06-10 05:08:09 +0800

    fixing error deployment- removing unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import KaraokePlayer from './KaraokePlayer'
 
 const App = () => {
   return (
     <div>
       <h1 style={{textAlign:'center'}}>My AI Karaoke App</h1>
-      <KaraokePlayer initialVideoId='tAEek8nO11c'/>
+      <KaraokePlayer/>
     </div>
   )
 }
